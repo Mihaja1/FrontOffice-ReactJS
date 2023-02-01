68bb2b27cde838bc4cf7dfb308c362a84e3a95b2
Mihaja1 <mihajaraman@gmail.com>
2023-02-01 20:19:34 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import {BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./pages/layout/Layout";
+import Header from "./component/Header";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Inscription from "./pages/Inscription";
+import MesEncheres from "./pages/MesEncheres";
+import Historique from "./pages/Historique";
 
 function App() {
   return (
@@ -13,6 +16,9 @@ function App() {
         <Route path="/" element={ <Layout /> } >
           <Route path="home" element={ <Home /> } />
         </Route>
+        <Route path="/liste" element={<MesEncheres/>}></Route>
+        <Route path="/historique" element={<Historique/>}></Route>
+        <Route path="/head" element={<Header/>}></Route>
       </Routes>
     </BrowserRouter>
   );
